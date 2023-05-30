4bc1e3075b9cf2cba3c1c623679417a428e14a21
Curve <fynnbwdt@gmail.com>
2023-05-30 21:55:53 +0200

    feat: update tests

--- tests/basic.test.cpp
+++ tests/basic.test.cpp
@@ -7,6 +7,11 @@ extern "C" void some_c_function(void (*callback)(const char *a, int b))
     callback("test", 1337);
 }
 
+extern "C" void some_c_function_with_user_data(void (*callback)(const char *a, int b, void *user_data), void *user_data)
+{
+    callback("test", 1337, user_data);
+}
+
 TEST_CASE("Basic usage is tested", "[Basic]")
 {
     int test = 0;
@@ -19,7 +24,7 @@ TEST_CASE("Basic usage is tested", "[Basic]")
 
     REQUIRE(test == 1);
 
-    some_c_function(lambda_ptr::pointer_to<void, const char *, int>([&](auto a, auto b) {
+    some_c_function(lambda_ptr::pointer_to<void(const char *, int)>([&](auto a, auto b) {
         REQUIRE(strcmp(a, "test") == 0);
         REQUIRE(b == 1337);
         test = 2;
@@ -27,7 +32,7 @@ TEST_CASE("Basic usage is tested", "[Basic]")
 
     REQUIRE(test == 2);
 
-    some_c_function(lambda_ptr::pointer_to<void(const char *, int)>([&](auto a, auto b) {
+    some_c_function(lambda_ptr::pointer_to<void (*)(const char *, int)>([&](auto a, auto b) {
         REQUIRE(strcmp(a, "test") == 0);
         REQUIRE(b == 1337);
         test = 3;
@@ -35,17 +40,20 @@ TEST_CASE("Basic usage is tested", "[Basic]")
 
     REQUIRE(test == 3);
 
-    some_c_function(lambda_ptr::pointer_to<void (*)(const char *, int)>([&](auto a, auto b) {
-        REQUIRE(strcmp(a, "test") == 0);
-        REQUIRE(b == 1337);
-        test = 4;
-    }));
+    auto user_data = lambda_ptr::user_data(test);
+
+    some_c_function_with_user_data(
+        [](const char *, int, void *data) {
+            auto &[test] = decltype(user_data)::from(data);
+            test = 4;
+        },
+        &user_data);
 
     REQUIRE(test == 4);
 
-    static_assert(lambda_ptr::detail::capture_lambda<decltype([&] {})>);
-    static_assert(lambda_ptr::detail::capture_lambda<decltype([=] {})>);
+    static_assert(lambda_ptr::detail::is_lambda<decltype([&] {})>);
+    static_assert(lambda_ptr::detail::is_lambda<decltype([=] {})>);
 
-    static_assert(not lambda_ptr::detail::capture_lambda<decltype([] {})>);
-    static_assert(not lambda_ptr::detail::capture_lambda<std::function<void(const char *, int)>>);
+    static_assert(not lambda_ptr::detail::is_lambda<decltype([] {})>);
+    static_assert(not lambda_ptr::detail::is_lambda<std::function<void(const char *, int)>>);
 }
